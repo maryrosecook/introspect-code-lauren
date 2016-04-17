f43c8082c899d5fa18a2161b9e81b95934acd6d1
Mary Rose Cook <mary@maryrosecook.com>
2016-04-17 11:06:10 +0100

    Pull more rendering code from controller to view

--- index.js
+++ index.js
@@ -1,74 +1,11 @@
-function highlight(key) {
-  clear();
-
-  var annotation = annotations[key];
-  if (annotation !== undefined) {
-    $('html,body').css('cursor', 'default');
-    $("#" + annotation.highlight).addClass("annotation-extremities");
-    $("#" + key).addClass("annotation-hot-area");
-    setExplanation(annotation.text);
-  }
-};
-
-function clearHighlight() {
-  $("#code span").removeClass("annotation-extremities");
-  $("#code span").removeClass("annotation-hot-area");
-};
-
-function clear() {
-  clearExplanation();
-  clearHighlight();
-};
-
-function setExplanation(explanation) {
-  $("#explanation").text(explanation);
-  $("#explanation").addClass("on");
-};
-
-function clearExplanation() {
-  $("#explanation").text("");
-  $("#explanation").removeClass("on");
-};
-
-var annotations = {
-  c: { text: "Creates a circle and names it 'black-circle'", highlight: "a" },
-  d: { text: "Creates a circle", highlight: "d" },
-  e: { text: "x coordinate of the circle", highlight: "e" },
-  f: { text: "y coordinate of the circle", highlight: "f" },
-  g: { text: "Radius of the circle", highlight: "g" },
-  h: { text: "Draw a thing", highlight: "h" },
-  i: { text: "The thing to draw", highlight: "i" },
-};
-
-function setupHovering() {
-  $("body").mousemove(function(e) {
-    clear();
-  });
-
-  Object.keys(annotations).forEach(function(key) {
-    $("#" + key).mousemove(function(e) {
-      if (state.get("explainingCode") === true) {
-        highlight(key);
-        e.stopPropagation();
-      }
-    });
-  });
-};
-
-function teardownHovering() {
-  $("#code").unbind("mousemove");
-
-  Object.keys(annotations).forEach(function(key) {
-    $("#" + key).unbind("mousemove");
-  });
-};
+// model
 
 function State(initialState) {
   var state = {};
 
   this.set = function(key, value) {
     state[key] = value;
-    updateButtonView(this);
+    updateView(this);
   };
 
   this.get = function(key) {
@@ -81,25 +18,94 @@ function State(initialState) {
   });
 };
 
+var annotations = {
+  c: { text: "Creates a circle and names it 'black-circle'", highlight: "a" },
+  d: { text: "Creates a circle", highlight: "d" },
+  e: { text: "x coordinate of the circle", highlight: "e" },
+  f: { text: "y coordinate of the circle", highlight: "f" },
+  g: { text: "Radius of the circle", highlight: "g" },
+  h: { text: "Draw a thing", highlight: "h" },
+  i: { text: "The thing to draw", highlight: "i" },
+};
+
+// view
+
+function updateView(state) {
+  updateButtonView(state)
+  updateCodeView(state);
+  updateExplanationView(state);
+};
+
 function updateButtonView(state) {
-  if (state.get("explainingCode") === true) {
-    $("#explain-code").addClass("on");
+  if (state.get("isExplaining") === true) {
+    $("#explain").addClass("on");
   } else {
-    $("#explain-code").removeClass("on");
+    $("#explain").removeClass("on");
   }
 };
 
-function clickExplainCode() {
-  if (state.get("explainingCode") === true) {
-    state.set("explainingCode", false);
-  } else {
-    state.set("explainingCode", true);
+function updateCodeView(state) {
+  clearCodeHighlight();
+
+  var hoveringOver = state.get("hoveringOver");
+  var isExplaining = state.get("isExplaining");
+  if (isExplaining && hoveringOver !== undefined) {
+    var annotation = annotations[hoveringOver];
+    if (annotation !== undefined) {
+      $('html,body').css('cursor', 'default');
+      $("#" + annotation.highlight).addClass("annotation-extremities");
+      $("#" + hoveringOver).addClass("annotation-hot-area");
+    }
   }
 };
 
-(function setupButtonClicking() {
-  $("#explain-code").click(clickExplainCode);
-})();
+function clearCodeHighlight() {
+  $("#code span").removeClass("annotation-extremities");
+  $("#code span").removeClass("annotation-hot-area");
+};
 
-var state = new State({ explainingCode: false });
+function updateExplanationView(state) {
+  var hoveringOver = state.get("hoveringOver");
+  var isExplaining = state.get("isExplaining");
+
+  if (isExplaining && hoveringOver !== undefined) {
+    $("#explanation").text(annotations[hoveringOver].text);
+  } else if (isExplaining) {
+    $("#explanation").text("Hover your mouse over something to see an explanation");
+  } else {
+    $("#explanation").text("");
+  }
+};
+
+// event handler functions
+
+function clickExplain() {
+  if (state.get("isExplaining") === true) {
+    state.set("isExplaining", false);
+  } else {
+    state.set("isExplaining", true);
+  }
+};
+
+function setupHovering() {
+  $("body").mousemove(function(e) {
+    state.set("hoveringOver", undefined);
+  });
+
+  Object.keys(annotations).forEach(function(key) {
+    $("#" + key).mousemove(function(e) {
+      state.set("hoveringOver", this.id);
+      e.stopPropagation();
+    });
+  });
+};
+
+function setupButtonClicking() {
+  $("#explain").click(clickExplain);
+};
+
+// setup app
+
+var state = new State({ isExplaining: false, hoveringOver: undefined });
 setupHovering();
+setupButtonClicking();
